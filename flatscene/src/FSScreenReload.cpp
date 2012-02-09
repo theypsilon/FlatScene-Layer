0d2104671097d3adf2580fd272dfac40fc797304
José Manuel Barroso <theypsilon@gmail.com>
2012-02-09 18:43:18 +0100

    Removed global objects in FSScreenReload.cpp

--- flatscene/src/FSScreenReload.cpp
+++ flatscene/src/FSScreenReload.cpp
@@ -4,19 +4,22 @@
 #include "FSImagesImpl.h"
 
 void FSScreen::saveResources(GraphicResources &info) {
+
+    FSImages& img = FSImages::I();
+    FSWriter& writer = FSWriter::I();
 	
-	for (SpritesetCollection::iterator it=Img._impl->set.begin(),kt=Img._impl->set.end();it!=kt;++it)	{
+	for (SpritesetCollection::iterator it=img._impl->set.begin(),kt=img._impl->set.end();it!=kt;++it)	{
 		SpritesetInfo aux;
 		aux.number = it->first;
 		aux.name =	it->second->getName();
 		aux.mode = it->second->getMode();
-		aux.times = Img.getCount(it->first);
+		aux.times = img.getCount(it->first);
 		info.push_back(aux);
 	}
 
-	Img.clear();
+	img.clear();
 
-	for (map<int,FSWriter::WriterImpl::SFont*>::iterator it=Write._impl->Fonts.begin();it!=Write._impl->Fonts.end();++it) {
+	for (map<int,FSWriter::WriterImpl::SFont*>::iterator it=writer._impl->Fonts.begin();it!=writer._impl->Fonts.end();++it) {
 		map<Uint16,FSCanvas*>& chars = it->second->render;
 		for (map<Uint16,FSCanvas*>::iterator jt=chars.begin();jt!=chars.end();++jt) {
 			imageToDelete.push_back(jt->second); // delete
@@ -24,7 +27,7 @@ void FSScreen::saveResources(GraphicResources &info) {
 		}
 	}
 
-	for (map<FSEngine*,FSWriter::WriterImpl::SData*>::iterator it=Write._impl->session.begin();it!=Write._impl->session.end();++it) {
+	for (map<FSEngine*,FSWriter::WriterImpl::SData*>::iterator it=writer._impl->session.begin();it!=writer._impl->session.end();++it) {
 		map<int,FSWriter::WriterImpl::FSText*>& auxBoxs = it->second->Texts;
 		for (map<int,FSWriter::WriterImpl::FSText*>::iterator jt=auxBoxs.begin();jt!=auxBoxs.end();++jt) {
 			if (jt->second->Type() == TT_BOX && jt->second->Box)
@@ -37,19 +40,22 @@ void FSScreen::saveResources(GraphicResources &info) {
 }
 
 void FSScreen::reloadResources(GraphicResources &info) {
+
+    FSImages& img = FSImages::I();
+    FSWriter& writer = FSWriter::I();
 	
-	for (map<int,FSWriter::WriterImpl::SFont*>::iterator it=Write._impl->Fonts.begin();it!=Write._impl->Fonts.end();++it) {
+	for (map<int,FSWriter::WriterImpl::SFont*>::iterator it=writer._impl->Fonts.begin();it!=writer._impl->Fonts.end();++it) {
 		map<Uint16,FSCanvas*>& chars = it->second->render;
 		for (map<Uint16,FSCanvas*>::iterator jt=chars.begin();jt!=chars.end();++jt) {
 			if (!jt->second) {
-				jt->second = new FSSprite(FSCanvas::toSCanvas(TTF_RenderGlyph_Blended(it->second->fuente,jt->first,Write._impl->data->fgcolor)));
+				jt->second = new FSSprite(FSCanvas::toSCanvas(TTF_RenderGlyph_Blended(it->second->fuente,jt->first,writer._impl->data->fgcolor)));
 			} else {
 				return FSLibrary::I().Error("No se puede recargar el recurso glyph porque no había sido descargado anteriormente.");
 			}
 		}
 	}
 
-	for (map<FSEngine*,FSWriter::WriterImpl::SData*>::iterator it=Write._impl->session.begin();it!=Write._impl->session.end();++it) {
+	for (map<FSEngine*,FSWriter::WriterImpl::SData*>::iterator it=writer._impl->session.begin();it!=writer._impl->session.end();++it) {
 		map<int,FSWriter::WriterImpl::FSText*>& auxBoxs = it->second->Texts;
 		for (map<int,FSWriter::WriterImpl::FSText*>::iterator jt=auxBoxs.begin();jt!=auxBoxs.end();++jt) {
 			if (jt->second->Type() == TT_BOX && jt->second->Box)
@@ -61,12 +67,12 @@ void FSScreen::reloadResources(GraphicResources &info) {
 	int aux = -1;
 
 	for (GraphicResources::iterator it=info.begin(),et=info.end();it!=et;++it) {
-		number = Img.add((*it).name.c_str(),(*it).mode);
+		number = img.add((*it).name.c_str(),(*it).mode);
 		if (number != it->number) {
-			Img._impl->set[it->number] = Img._impl->set[number];
-			Img._impl->set.erase(number);
-			Img._impl->lastIndexAdded.push(number);
+			img._impl->set[it->number] = img._impl->set[number];
+			img._impl->set.erase(number);
+			img._impl->lastIndexAdded.push(number);
 		}
-		Img._impl->count[Img.get(it->number)]=it->times;
+		img._impl->count[img.get(it->number)]=it->times;
 	}
 }
