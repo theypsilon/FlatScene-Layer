bdac3a5a276eb8cc3eb3ca7edf2a5ff49a590bed
José Manuel Barroso <theypsilon@gmail.com>
2012-02-20 11:39:22 +0100

    Formatting XMLHelper file

--- flatscene/src/XMLHelper.h
+++ flatscene/src/XMLHelper.h
@@ -5,42 +5,37 @@
 #include "FSparserXML.h"
 #include <string>
 
-namespace fs {
-    namespace intern {
-        namespace xml {
+namespace fs { namespace intern { namespace xml {
 
-            bool checkAttr(const TiXmlElement& el,const char *const name, const char *const value = nullptr, bool equals = true) {
-                if (!name) throw FSException("no attribute name supplied",__LINE__);
-                auto attr = el.Attribute(name);
-                if (!attr) return false;
-                if (value && ( ( equals && strcmp(attr,value)!=0) || 
-                               (!equals && strcmp(attr,value)==0) )) 
-                               return false;
-                return true;
-            }
-
-            void ensureAttr(const TiXmlElement& el,const char *const name, const char *const value = nullptr, bool equals = true) {
-                if (!checkAttr(el,name,value,equals))  throw FSException( std::string("invalid attribute '")+
-                        (name?name:"nullptr")+"' with value '"+ (value?value:"nullptr")+"' equals="+(equals?"true":"false"));
-            }
-
-            int intFromAttr(const TiXmlElement& el,const char *const name) {
-                ensureAttr(el,name);
-                int ret;
-                el.QueryIntAttribute(name,&ret);
-                return ret;
-            }
-
-            int intFromAttr(const TiXmlElement& el,const char *const name,int min, int max) {
-                ensureAttr(el,name);
-                int ret;
-                el.QueryIntAttribute(name,&ret);
-                if (ret < min || ret > max) 
-                    throw FSException(std::string("int '")+name+"' not in range",__LINE__);
-                return ret;
-            }
-        }
+    bool checkAttr(const TiXmlElement& el,const char *const name, const char *const value = nullptr, bool equals = true) {
+        if (!name) throw FSException("no attribute name supplied",__LINE__);
+        auto attr = el.Attribute(name);
+        if (!attr) return false;
+        if (value && ( ( equals && strcmp(attr,value)!=0) || 
+                       (!equals && strcmp(attr,value)==0) )) 
+                       return false;
+        return true;
     }
-}
+
+    void ensureAttr(const TiXmlElement& el,const char *const name, const char *const value = nullptr, bool equals = true) {
+        if (!checkAttr(el,name,value,equals))  throw FSException( std::string("invalid attribute '")+
+                (name?name:"nullptr")+"' with value '"+ (value?value:"nullptr")+"' equals="+(equals?"true":"false"));
+    }
+
+    int intFromAttr(const TiXmlElement& el,const char *const name) {
+        ensureAttr(el,name);
+        int ret;
+        el.QueryIntAttribute(name,&ret);
+        return ret;
+    }
+
+    int intFromAttr(const TiXmlElement& el,const char *const name,int min, int max) {
+        int ret = intFromAttr(el,name);
+        if (ret < min || ret > max) 
+            throw FSException(std::string("int '")+name+"' not in range",__LINE__);
+        return ret;
+    }
+
+}}}
 
 #endif
