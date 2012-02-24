1cfd279459ff7effa54e4f02a6621d1d0d0ced72
José Manuel Barroso <theypsilon@gmail.com>
2012-02-24 01:02:01 +0100

    Trying to draw something without success

--- itemology/main.cpp
+++ itemology/main.cpp
@@ -13,6 +13,19 @@ int main (int argc, char* argv[]) {
 
     auto engine = unique_ptr<IFirstEngine>(new IFirstEngine);
 
+    int e = Img.add("resources/tilp");
+
+    auto spt = Img.get(e)->get(10);
+
+    FSScreen& screen = FSScreen::I();
+
+    for (;;) {
+        screen.locateRenderScene(0,0,640,480);
+        for (int i=0;i<100;i++)
+            spt->put((10*(i%10),(i/10)*10));
+        screen.render();
+    }
+
     FSLib.processEngine(move(engine));
 
     cout << "Ending main" << endl;
