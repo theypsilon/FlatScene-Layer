9605c271166c8139993fb049b309e70e56b0a303
José Manuel Barroso <theypsilon@gmail.com>
2012-02-25 21:29:23 +0100

    Centering image

--- itemology/main.cpp
+++ itemology/main.cpp
@@ -16,17 +16,17 @@ int main (int argc, char* argv[]) {
     FSPoint pt(0,0);
 
     auto surf = IMG_Load("resources/tilp.png");
+    FSSprite chipset(FSCanvas::toSCanvas(surf,WITH_SDL_SURFACE),pt);
 
     int index = Img.add("resources/tilp",WITH_SDL_SURFACE);
     auto& spt = *Img.get(index)->get(0);
-    FSSprite chipset(FSCanvas::toSCanvas(surf,WITH_SDL_SURFACE),pt);
     for (int timer = SDL_GetTicks() ; timer + 4000 > SDL_GetTicks();) {
             FSDraw.projectionMode(TRP_PERSPECTIVE,1600);
             FSDraw.locateRenderScene(0,0,640,480);
-            pt.set(0,0);
+            pt.set(198,64);
             chipset.put(pt);
             for (int i = 0;i < 100; i++) {
-                pt.set((i%20)*32,320+(i/20)*32);
+                pt.set((i%20)*32,240+(i/20)*32);
                 spt.put(pt,0);
             }
             FSDraw.render();
