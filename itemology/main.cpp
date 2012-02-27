789a0df26ee5c9aa22827651f7a66b6602f73536
José Manuel Barroso <theypsilon@gmail.com>
2012-02-28 00:16:06 +0100

    main invoking engine

--- itemology/main.cpp
+++ itemology/main.cpp
@@ -12,28 +12,8 @@ int main (int argc, char* argv[]) {
     //run the application
     FSLib.startLibrary(false);
     FSDraw.start(640,480,32,1.0,1.0,false);
-    Write.loadFont("resources/tahoma");
-    FSPoint pt(0,0);
 
-    auto surf = IMG_Load("resources/tilp.png");
-    FSSprite chipset(FSCanvas::toSCanvas(surf,WITH_SDL_SURFACE),pt);
-
-    int index = Img.add("resources/tilp",WITH_SDL_SURFACE);
-    auto& spt = *Img.get(index)->get(4);
-    for (int timer = SDL_GetTicks() ; timer + 4000 > SDL_GetTicks();) {
-            FSDraw.projectionMode(TRP_PERSPECTIVE,1600);
-            FSDraw.locateRenderScene(0,0,640,480);
-            pt.set(198,64);
-            chipset.put(pt);
-            for (int i = 0;i < 100; i++) {
-                pt.set((i%20)*32,240+(i/20)*32);
-                spt.put(pt,0);
-            }
-            FSDraw.render();
-            SDL_Delay(16);
-    }
-    
-    return(0);
+    FSLib.processEngine(unique_ptr<IFirstEngine>(new IFirstEngine));
 
     cout << "Ending main" << endl;
 
