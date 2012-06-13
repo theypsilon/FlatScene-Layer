3e3063a0ad72146b5461054a0a6c175e2a3c18ab
José Manuel Barroso <theypsilon@gmail.com>
2012-06-13 22:03:31 +0200

    Removed type from FSText

--- flatscene/src/FSWriterImpl.h
+++ flatscene/src/FSWriterImpl.h
@@ -120,7 +120,6 @@ struct Writer::WriterImpl {
 
     class FSText {
     private:
-        TypeText type;
         FSText(const FSText&) {}
         FSText& operator=(const FSText&) { return *this; }
     public:
@@ -132,7 +131,6 @@ struct Writer::WriterImpl {
         FSText(FSText&& mv);
         FSText& operator=(FSText&& rhs);
         ~FSText();
-        TypeText Type() const;
     };
 
     struct SData {
