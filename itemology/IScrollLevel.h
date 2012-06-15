7e5f12dc483f1a4404368e4952ee323ca78de19e
José Manuel Barroso <theypsilon@gmail.com>
2012-06-15 19:54:45 +0200

    Better code in IScrollLevel

--- itemology/IScrollLevel.h
+++ itemology/IScrollLevel.h
@@ -7,89 +7,86 @@
 #include "Tile.h"
 
 struct TileBG {
-    Uint8 graph;
-    Uint8 fileGraph;
-    Uint16 flags;
-    Uint8 dur;
-    Uint8 fileDur;
-    Uint16 vacio;
+    unsigned char   graph;
+    unsigned char   fileGraph;
+    unsigned short  flags;
+    unsigned char   dur;
+    unsigned char   fileDur;
+    unsigned short  vacio;
 };
 
 struct ITileAndDur {
-    std::vector<std::vector<Tile> > tiles;
-    bool dur;
+    std::vector<
+    std::vector<Tile> > tiles;
+    bool                dur;
 };
 
 struct gate {
-    std::string destino;
-    Uint32 regionx1;
-    Uint32 regionx2;
-    Uint32 regiony1;
-    Uint32 regiony2;
-    Uint32 regionz;
-    Uint32 destino_scroll_x;                
-    Uint32 destino_scroll_y;
-    Uint32 destino_scroll_z;
+    std::string     destino;
+    unsigned int    regionx1;
+    unsigned int    regionx2;
+    unsigned int    regiony1;
+    unsigned int    regiony2;
+    unsigned int    regionz;
+    unsigned int    destino_scroll_x;
+    unsigned int    destino_scroll_y;
+    unsigned int    destino_scroll_z;
 };
 
 
 
-class IScrollLevel :
-    public Universe
-{
-private:
-
-    Uint32 mapHeight;
-    Uint32 mapWidth;
-    Uint32 numLayers;
-
-    // dimension de tiles.
-    Uint32 tileHeight;
-    Uint32 tileWidth;
-
-    std::string datosTile;
-    std::string datosSong;
-
-    std::vector<Spriteset> _tilesets;
-    std::vector<Spriteset> _collisionsets;
-
-    std::vector<ITileAndDur> _layerlvl;
-    std::vector<
-        std::vector<
-            std::vector<Tile>> const*> _linktodur;
-
-    std::vector<Uint32> LayerFloor;
-    std::vector<Uint32> LayerType;
-
-    Uint32 numGates;
-
-    std::vector<gate> Gates;
-
-    std::vector<
-        std::vector<
-            std::vector<
-                std::list<IScrollObject*>>>> _MA;
-
+class IScrollLevel : public Universe {
     friend class IScrollCamera;
 
+    unsigned int                    mapHeight;
+    unsigned int                    mapWidth;
+    unsigned int                    numLayers;
+
+    // dimension de tiles.
+    unsigned int                    tileHeight;
+    unsigned int                    tileWidth;
+
+    std::string                     datosTile;
+    std::string                     datosSong;
+
+    std::vector<Spriteset>          _tilesets;
+    std::vector<Spriteset>          _collisionsets;
+
+    std::vector<ITileAndDur>        _layerlvl;
+    std::vector<
+    std::vector<
+    std::vector<Tile>> const*>      _linktodur;
+
+    std::vector<Uint32>             LayerFloor;
+    std::vector<Uint32>             LayerType;
+
+    Uint32                          numGates;
+
+    std::vector<gate>               Gates;
+
+    std::vector<
+    std::vector<
+    std::vector<
+    std::list  <IScrollObject*>>>>  _MA;
+
 public:
-    IScrollLevel(std::string name);
-    ~IScrollLevel(void);
+                                    IScrollLevel(std::string name);
+                                    ~IScrollLevel();
 
-    virtual void load();
-    virtual void unload();
+    virtual void                    load();
+    virtual void                    unload();
 
-    virtual int incActor(Actor* act);
-    virtual int decActor(Actor* act);
+    virtual int                     incActor(Actor* act);
+    virtual int                     decActor(Actor* act);
 
-    virtual Uint32 getPixel(int x, int y, int z);
+    virtual unsigned int            getPixel(int x, int y, int z);
 
-    virtual Uint32 getW() { return mapWidth; };
-    virtual Uint32 getH() { return mapHeight; };
-    virtual Uint32 getTileW() { return tileWidth; };
-    virtual Uint32 getTileH() { return tileHeight; };
+    virtual unsigned int            getW() { return mapWidth; };
+    virtual unsigned int            getH() { return mapHeight; };
+    virtual unsigned int            getTileW() { return tileWidth; };
+    virtual unsigned int            getTileH() { return tileHeight; };
 
-    const std::vector<Spriteset>& getTileset() { return _tilesets; };
+    const std::vector<Spriteset>&   getTileset() { return _tilesets; };
 };
 
 #endif
