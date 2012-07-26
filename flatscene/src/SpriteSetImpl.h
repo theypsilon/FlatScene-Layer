825cc159e1cf846a7f2808918ffdcc426208ccf4
José Manuel Barroso <theypsilon@gmail.com>
2012-07-26 18:36:27 +0200

    Have to go to party

--- flatscene/src/SpriteSetImpl.h
+++ flatscene/src/SpriteSetImpl.h
@@ -14,6 +14,8 @@
 #include "CanvasFactory.h"
 #include "SpriteImpl.h"
 
+#include "GraphicResourceDescriptor.h"
+
 namespace FlatScene {
 
 using namespace Util::XML::Tiny;
@@ -63,15 +65,13 @@ struct Spriteset::SpritesetImpl {
     }
 
     void loadChipset(std::string& c,Uint8 mode=ONLY_TEXTURE,std::string* cPrev=NULL) {
-        std::string grd_str, chipset_str;
+        auto names = getNameFile(c);
 
-        getNameFile(c,grd_str,chipset_str);
-
-        auto chipset = IMG_Load(chipset_str.c_str());
+        auto chipset = IMG_Load(names.second.c_str());
         if (!chipset)
             throw Exception("chipset couldn't load");
 
-        auto grd = loadFileGRD(grd_str,chipset);
+        auto grd = loadFileGRD(names.first,chipset);
 
         loadAllSprites(grd,*chipset,mode);
 
@@ -82,215 +82,29 @@ struct Spriteset::SpritesetImpl {
         return bitmap == ".png" || bitmap == ".bmp" || bitmap == ".jpg";
     }
 
-    void getNameFile(const std::string& str, std::string& grd, std::string& bitmap) {
+    std::pair<std::string, std::string> getNameFile(const std::string& str) {
 
         std::string tipefile;
         std::string namefile;
 
         auto res = str.end();
-        for(auto it=str.begin(), et=str.end(); it != et ; ++it)
+        for(auto it=str.end(); it != str.begin() ; --it)
             if (*it == '.')
                 res = it;
 
         if (res != str.end()) {
-            namefile = std::string(str.begin(),res);
-            tipefile = std::string(res,str.end());
+            namefile += std::string(str.begin(),res);
+            tipefile += std::string(res,str.end());
         } else {
-            namefile = str;
-            tipefile = ".png";
+            namefile += str;
+            tipefile += ".png";
         }
 
         if (!isValidBitmapExtension(tipefile)) 
             throw Exception("graphic bitmap format not valid");
 
-        grd = namefile + ".grd";
-        bitmap = namefile + tipefile;
+        return std::make_pair(namefile + ".grd",namefile + tipefile);
 
-    }
-
-    struct DataGRD {
-        typedef std::vector<Rectangle> Area;
-        typedef Point2D<unsigned int>  CPoint;
-        typedef Point2D<unsigned int>  DimPoint;
-        struct Sprite {
-            std::string         name;
-            DimPoint            dim;
-            CPoint              cp;
-            std::map<int,Area>  areas;
-            std::map<int,bool>  a_isRel;
-        };
-
-        std::vector<Sprite> images;
-        unsigned int        num_img;
-        unsigned int        cellwidth;
-        unsigned int        cellheight;
-        bool                simple;
-        double              sp_scale;
-        CPoint              globalcp;
-        std::map<int,Area>  globalareas;
-        std::map<int,bool>  ga_isRel;
-    };
-
-    struct DocIsNotLoadedException {};
-
-    DataGRD loadFileGRD(const std::string& grd_str, const SDL_Surface *const chipset = nullptr) {
-        try {
-            return fillGRDFromDocument(grd_str);
-        } catch(DocIsNotLoadedException& e) {
-            if (!chipset) 
-                throw Exception("grd file invalid and bitmap invalid",__LINE__);
-
-            return fillGRDFromChipset(*chipset);
-        }
-    }
-
-    TiXmlDocument getLoadedDocument(const std::string& str) {
-        TiXmlDocument doc(str.c_str());
-        if (!doc.LoadFile())
-            throw DocIsNotLoadedException();
-
-        if (!doc.FirstChild()->ToElement()) 
-            throw Exception("no elements in given document",__LINE__);
-
-        return doc;
-    }
-
-    DataGRD fillGRDFromDocument(const std::string& grd_str) {
-        TiXmlDocument doc = getLoadedDocument(grd_str);
-        TiXmlHandle input(doc.FirstChild()); 
-
-        int i = 0;
-        while (isDefinedInOtherFile(*input.Element())) {
-            if (i++ > 100) throw Exception("100 limit cycles exceeded between grd files",__LINE__);
-            doc = getFromOtherFile(*input.Element());
-            input = TiXmlHandle(doc.FirstChild());
-        }
-
-        DataGRD grd;
-        processHeadElement(grd,*input.Element());
-        if (grd.simple) 
-            return grd;
-
-        processGlobalValues(grd,input);
-        processSpriteValues(grd,input);
-
-        ensureConsistentValues(grd);
-        return grd;
-    }
-
-    DataGRD fillGRDFromChipset(const SDL_Surface& chipset) {
-        DataGRD grd;
-        grd.sp_scale = 1.0;
-        grd.num_img = 1;
-        grd.simple = true;
-        grd.cellwidth  = chipset.w;
-        grd.cellheight = chipset.h;
-        return grd;
-    }
-
-    template <typename PointType> 
-    void fillAreasFromElement(const TiXmlElement* pArea, std::map<int,DataGRD::Area>& areas, 
-                                const PointType& cp, double scale, std::map<int,bool>& rel_m ) {
-        for ( ; pArea ; pArea = pArea->NextSiblingElement()) {
-            int id = numFromAttr(*pArea,"id",0);
-            std::remove_reference<decltype(areas.at(0))>::type area;
-
-            bool rel = checkAttr(*pArea,"relative","true");
-            for (auto pRect = pArea->FirstChildElement("rectangle"); 
-                    pRect; pRect = pRect->NextSiblingElement() )
-                    area.push_back
-                      (((int)(scale * (intFromAttr(*pRect,"x1") + (rel? cp.x : 0)) ) ,
-                        (int)(scale * (intFromAttr(*pRect,"y1") + (rel? cp.y : 0)) ) ,
-                        (int)(scale * (intFromAttr(*pRect,"x2") + (rel? cp.x : 0)) ) ,
-                        (int)(scale * (intFromAttr(*pRect,"y2") + (rel? cp.y : 0)))));
-
-            areas[id] = std::move(area);
-            rel_m[id] = rel;
-        }
-    }
-
-    bool isDefinedInOtherFile(const TiXmlElement& head) {
-        return checkAttr(head,"defined-in","",false);
-    }
-
-    TiXmlDocument getFromOtherFile(const TiXmlElement& head) {
-        ensureAttr(head,"defined-in","",false);
-        std::string grd_str(head.Attribute("defined-in"));
-        return getLoadedDocument(grd_str);
-    }
-
-    void processHeadElement(DataGRD& grd, const TiXmlElement& head) {
-        grd.num_img    = numFromAttr<decltype(grd.num_img)   >(head,"sprites");
-        grd.cellwidth  = numFromAttr<decltype(grd.cellwidth) >(head,"cellwidth");
-        grd.cellheight = numFromAttr<decltype(grd.cellheight)>(head,"cellheight");
-
-        //if (checkAttr(head,"type","split"))
-        //    ; // @TODO return loadChipsetSplit(s_aux,mode);
-
-        grd.simple = checkAttr(head,"simple","true");
-        if (checkAttr(head,"sp-scale"))
-            grd.sp_scale = numFromAttr<decltype(grd.sp_scale)>(head,"sp-scale", 0.01, 100);
-        else
-            grd.sp_scale = 1.0;
-    }
-
-    void processGlobalValues(DataGRD& grd, const TiXmlHandle& doc) {
-        if (auto el = doc.FirstChildElement("globalcpoint").Element()) {
-            grd.globalcp.set(
-                numFromAttr<decltype(grd.globalcp.x)>(*el,"x",0,grd.cellwidth),
-                numFromAttr<decltype(grd.globalcp.y)>(*el,"y",0,grd.cellheight)
-            );
-        } else {
-            grd.globalcp.set(0, 0);
-        }
-
-        fillAreasFromElement(
-            doc.FirstChildElement("globalareas").FirstChildElement("area").ToElement(),
-            grd.globalareas,
-            grd.globalcp,
-            grd.sp_scale,
-            grd.ga_isRel
-        );
-    }
-
-    void processSpriteValues(DataGRD& grd, const TiXmlHandle& doc) {
-        for (auto pImg = doc.FirstChildElement("img").ToElement(); 
-            pImg ; pImg = pImg->NextSiblingElement()) {
-                
-            DataGRD::Sprite spt; const auto& img = *pImg;
-            spt.name = checkAttr(img,"name","",false) ? 
-                valFromAttr<std::string>(img,"name") : "noname" ;
-            spt.dim.set( numFromAttr<decltype(spt.dim.x)>(img,"width",0,grd.cellwidth  ) ,
-                            numFromAttr<decltype(spt.dim.y)>(img,"height",0,grd.cellheight));
-
-            spt.cp.set( grd.globalcp.x, grd.globalcp.y );
-            if (auto el = img.FirstChildElement("cpoint")) {
-                spt.cp.x += numFromAttr<decltype(spt.cp.x)>(*el,"x",0, spt.dim.x - grd.globalcp.x );
-                spt.cp.y += numFromAttr<decltype(spt.cp.y)>(*el,"y",0, spt.dim.x - grd.globalcp.y );
-            } else if (spt.cp.x > spt.dim.x || spt.cp.y > spt.dim.y)
-                throw Exception("the global cp is not valid due to image sizes",__LINE__);
-                
-            fillAreasFromElement(
-                img.FirstChildElement("area"), 
-                spt.areas, 
-                spt.cp, 
-                grd.sp_scale, 
-                spt.a_isRel
-            );
-
-            grd.images.push_back(std::move(spt));
-        }
-    }
-        
-    void ensureConsistentValues(const DataGRD& grd) {
-        if (grd.simple) return;
-
-        if (grd.num_img != grd.images.size())
-            throw Exception("image count doesn't match in grd file",__LINE__);
-
-        for (auto& img : grd.images) for (auto& ar : img.areas) for (auto& rc : ar.second)
-            if (rc.x < 0 || rc.y < 0 || rc.w > (decltype(rc.w))img.dim.x || rc.h > (decltype(rc.h))img.dim.y)
-                throw Exception("areas not defined within the sprite domain",__LINE__);
     }
 
     void loadAllSprites(DataGRD& grd, const SDL_Surface& chipset, unsigned char mode) {
