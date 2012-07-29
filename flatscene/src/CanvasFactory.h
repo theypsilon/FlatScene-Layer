1772da08b550bfbb510a86767de8ca5a8b9b3143
José Manuel Barroso <theypsilon@gmail.com>
2012-07-29 14:52:19 +0200

    Changing scaleSurface from factory file to sdlfunc file

--- flatscene/src/CanvasFactory.h
+++ flatscene/src/CanvasFactory.h
@@ -6,56 +6,6 @@
 
 namespace FlatScene {
 
-    SDL_Surface* scaleSurface( SDL_Surface* s_surf, int factor) {
-
-        SDL_Surface* ret = NULL;
-
-        if (s_surf == NULL || factor <= 1)
-            return ret;
-
-        unsigned char bpp = s_surf->format->BytesPerPixel;
-
-        if (bpp == 4) // 32 bits
-            ret = SDL_CreateRGBSurface(s_surf->flags, s_surf->w * factor, s_surf->h * factor, bpp*8,
-                s_surf->format->Rmask, s_surf->format->Gmask,
-                s_surf->format->Bmask, s_surf->format->Amask
-            );
-        else if (bpp == 1) // 8 bits
-            ret = SDL_CreateRGBSurface(s_surf->flags, s_surf->w * factor, s_surf->h * factor, bpp*8, 0,0,0,0 );
-        else 
-            throw SDLException("depth mode not valid");
-
-        
-
-        char* newPixels = (char*) ret->pixels;
-        char* oldPixels = (char*) s_surf->pixels;
-
-        SDL_LockSurface(ret);
-        SDL_LockSurface(s_surf);
-
-        for (int y = 0; y < s_surf->h; y++) {
-            for (int x = 0; x < s_surf->w; x++) {
-
-                int pos_old = y * s_surf->pitch + x * bpp;
-
-                for (int fx = 0; fx < factor; fx++) {
-                    for (int fy = 0; fy < factor; fy++) {
-
-                        int pos_new = (y*factor + fy) * ret->pitch + (x*factor +fx) * bpp;
-
-                        for (int b = 0; b < bpp; b++)
-                            newPixels [pos_new + b] =oldPixels [pos_old + b];
-                    }
-                }
-            }
-        }
-
-        SDL_UnlockSurface(s_surf);
-        SDL_UnlockSurface(ret);
-
-        return ret;
-    }
-
     SDL_Surface* loadSurface(const SDL_Rect& src, const SDL_Surface& chipset, unsigned char mode, double sp_scale) {
         auto surf = SDL_CreateRGBSurface(chipset.flags | SDL_SRCALPHA,
                                          src.w - src.x, src.h - src.y,
